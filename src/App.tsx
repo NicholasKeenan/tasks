ed193d3a6a5705d47fcc8a5c24200bc70e59dbec
NicholasKeenan <nicholas.keenan@maine.edu>
2026-01-29 17:55:30 -0500

    added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Nicholas Keenan
+                automatically reload. Nicholas Keenan Hello World
             </p>
         </div>
     );
