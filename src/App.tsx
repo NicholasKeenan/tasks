8d194c80913e390c2a87da91fa06ba3aa26b9883
NicholasKeenan <nicholas.keenan@maine.edu>
2026-02-09 11:22:13 -0500

    Columns III

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ function App(): React.JSX.Element {
                 <Row>
                     <Col>First Collumn.</Col>
                     <Col>
+                        Second column.
                         <h1>New heading</h1>
-
                         <Button
                             onClick={() => {
                                 console.log("I am logged");
@@ -22,20 +22,16 @@ function App(): React.JSX.Element {
                         >
                             Click Me
                         </Button>
-
                         <img
                             src="https://upload.wikimedia.org/wikipedia/commons/0/08/Corl0207_%2828225976491%29.jpg"
                             alt="A dope shark"
                         />
-
                         <h2>Why this shark is so cool</h2>
-
                         <ol>
                             <li>1. Just look at it</li>
                             <li>2. It is super fast</li>
                             <li>3. It has probably never killed anyone</li>
                         </ol>
-
                         <p>
                             Edit <code>src/App.tsx</code> and save. This page
                             will automatically reload. Nicholas Keenan Hello
