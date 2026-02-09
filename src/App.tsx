54cc18a6656cf55a360b8a899c3829193eeb9c20
NicholasKeenan <nicholas.keenan@maine.edu>
2026-02-09 11:18:17 -0500

    Columns again

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Button, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -11,33 +11,37 @@ function App(): React.JSX.Element {
 
             <Container>
                 <Row>
-                    <h1>New heading</h1>
+                    <Col>First Collumn.</Col>
+                    <Col>
+                        <h1>New heading</h1>
 
-                    <Button
-                        onClick={() => {
-                            console.log("I am logged");
-                        }}
-                    >
-                        Click Me
-                    </Button>
+                        <Button
+                            onClick={() => {
+                                console.log("I am logged");
+                            }}
+                        >
+                            Click Me
+                        </Button>
 
-                    <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Corl0207_%2828225976491%29.jpg"
-                        alt="A dope shark"
-                    />
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Corl0207_%2828225976491%29.jpg"
+                            alt="A dope shark"
+                        />
 
-                    <h2>Why this shark is so cool</h2>
+                        <h2>Why this shark is so cool</h2>
 
-                    <ol>
-                        <li>1. Just look at it</li>
-                        <li>2. It is super fast</li>
-                        <li>3. It has probably never killed anyone</li>
-                    </ol>
+                        <ol>
+                            <li>1. Just look at it</li>
+                            <li>2. It is super fast</li>
+                            <li>3. It has probably never killed anyone</li>
+                        </ol>
 
-                    <p>
-                        Edit <code>src/App.tsx</code> and save. This page will
-                        automatically reload. Nicholas Keenan Hello World
-                    </p>
+                        <p>
+                            Edit <code>src/App.tsx</code> and save. This page
+                            will automatically reload. Nicholas Keenan Hello
+                            World
+                        </p>
+                    </Col>
                 </Row>
             </Container>
         </div>
