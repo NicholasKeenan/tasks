095279a08a192e82c64a68a0250148ece05423b6
NicholasKeenan <nicholas.keenan@maine.edu>
2026-02-09 10:29:21 -0500

    added second header and image

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,13 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <h1>New heading</h1>
+
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Corl0207_%2828225976491%29.jpg"
+                alt="A dope shark"
+            />
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Nicholas Keenan Hello World
